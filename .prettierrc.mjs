b3c7b4c683457cf1e96d2f10bd787282ee79efa3
sebas0811buitrago <86497666+sebas0811buitrago@users.noreply.github.com>
2024-10-28 21:47:58 -0500

    chore: adding configurations

---
+++ .prettierrc.mjs
@@ -0,0 +1,26 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  quoteProps: "consistent",
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true,
+  endOfLine: "lf",
+  arrowParens: "always",
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
